416e11f90e023b509da75645613a2d109467b20b
Asya0 <anya.gerasimenko763@mail.ru>
2026-01-09 23:07:12 +0600

    feat: в jsonplaceholder только получает список задач, но не изменяет его. Попробовала mockapi.io, но он без впн не работает, плэтому решила использовать JSON Server для практики crud

--- src/context/TaskContext.jsx
+++ src/context/TaskContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useReducer, useContext, useEffect, useCallback  } from "react";
+import { createContext, useReducer, useContext, useEffect, useCallback } from "react";
 import { taskService } from "../services/taskService";
 
 const TaskStateContext = createContext(null);
@@ -7,7 +7,7 @@ const TaskDispatchContext = createContext(null);
 // Функция для инициализации состояния из localStorage
 
 function taskReducer(state, action) {
-  switch(action.type) {
+  switch (action.type) {
     // case "SET_TASKS": {
     //   return action.payload;
     // }
@@ -15,60 +15,69 @@ function taskReducer(state, action) {
       return action.payload || [];
     }
     case "ADD_TASK": {
-      const {title, priority} = action.payload;
+      const { title, priority } = action.payload;
 
       const newTask = {
         id: crypto.randomUUID(),
         title,
         isCompleted: false,
-        status: "not_started",
-        priority,
-        createdAt: Date.now(),
+        // status: "not_started",
+        // priority,
+        // createdAt: Date.now(),
       };
       return [newTask, ...state];
     }
     case "EDIT_TASK": {
-      const {id, title} = action.payload;
-      
-      return state.map(t => t.id === id 
-        ? { ...t, title} 
+      const { id, title } = action.payload;
+
+      return state.map(t => t.id === id
+        ? { ...t, title }
         : t);
     }
     case "DELETE_TASK": {
-      const {id} = action.payload;
+      const { id } = action.payload;
       return state.filter(t => t.id !== id);
     }
     case "CHECK_TASK": {
-      const {id} = action.payload;
+      const { id } = action.payload;
 
-      return state.map(t => t.id === id 
+      return state.map(t => t.id === id
         ? {
-            ...t,
-            isCompleted: !t.isCompleted,
-            status: !t.isCompleted ? "done" : "not_started"
-          }
+          ...t,
+          isCompleted: !t.isCompleted,
+          status: !t.isCompleted ? "done" : "not_started"
+        }
         : t
       );
     }
     case "MOVE_TASK": {
-      const {id, status} = action.payload;
+      const { id, status } = action.payload;
 
-      return state.map(t => t.id === id 
+      return state.map(t => t.id === id
         ? {
-            ...t,
-            status: status,
-            isCompleted: status === "done"
-          }
+          ...t,
+          status: status,
+          isCompleted: status === "done"
+        }
         : t
       );
     }
-    default: 
+    default:
       return state;
   }
 }
 
-export function TaskProvider({children}) {
-    const [state, dispatch] = useReducer(taskReducer, []);
+export function TaskProvider({ children }) {
+  const [state, dispatch] = useReducer(taskReducer, []);
+
+  const formatTaskFromApi = (apiTask) => ({
+    id: apiTask.id,
+    title: apiTask.title,
+    isCompleted: apiTask.completed,
+    status: apiTask.completed ? "done" : "not_started",
+    priority: "medium", // пока хардкод
+    createdAt: new Date().getTime(),
+  });
 
   useEffect(() => {
     const loadTasks = async () => {
@@ -76,63 +85,65 @@ export function TaskProvider({children}) {
         const tasks = await taskService.getAll();
         console.log(tasks, "задачи из апишки")
 
-        const formattedTasks = tasks.map(task => ({
-          id: task.id,
-          title: task.title,
-          isCompleted: task.completed,
-          status: task.completed ? "done" : "not_started",
-          priority: "medium",
-          createdAt: new Date().getTime(),
-        }));
-        dispatch({type: "SET_TASKS", payload: formattedTasks })
+        const formattedTasks = tasks.map(formatTaskFromApi);
+        dispatch({ type: "SET_TASKS", payload: formattedTasks })
       }
       catch (error) {
         console.log("Error to load tasks", error)
       }
     }
     loadTasks();
-  }, []) 
-  
+  }, [])
+
   const apiDispatch = useCallback(async (action) => {
-    try{
-      switch(action.type) {
+    try {
+      switch (action.type) {
         case "ADD_TASK":
-          const newTask = await taskService.create(action.payload)
-          dispatch({type: "ADD_TASK", payload: newTask})
+          const newTask = await taskService.createTask(action.payload);
+          console.log(newTask, "новая задача");
+          const formattedTask = {
+            id: apiTask.id, // или свой уникальный ID
+            title: apiTask.title,
+            isCompleted: false, // новая задача всегда не выполнена
+            status: "not_started",
+            priority: action.payload.priority || "medium", // берем из action.payload
+            createdAt: Date.now()
+          };
+          dispatch({ type: "ADD_TASK", payload: formattedTask })
           break;
 
-          //case "EDIT_TASK:..."
-          default: dispatch(action);
+        //case "EDIT_TASK:..."
+        default: dispatch(action);
       }
     }
-    catch(error) {
+    catch (error) {
       console.log(`Failed to ${action.type}:`, error)
     }
-  })
+  }, [])
 
-    return(
-        <TaskStateContext.Provider value={state}>
-            <TaskDispatchContext.Provider value={apiDispatch}>
-                {children}
-            </TaskDispatchContext.Provider>
-        </TaskStateContext.Provider>
-    )
+  return (
+    <TaskStateContext.Provider value={state}>
+      <TaskDispatchContext.Provider value={apiDispatch}>
+        {children}
+      </TaskDispatchContext.Provider>
+    </TaskStateContext.Provider>
+  )
 }
 
 export function useTasks() {
-    const context = useContext(TaskStateContext);
-    if (context === undefined) {
-        throw new Error("useTasks must be used within a TaskProvider");
-    }
-    return context || [];
+  const context = useContext(TaskStateContext);
+  if (context === undefined) {
+    throw new Error("useTasks must be used within a TaskProvider");
+  }
+  return context || [];
 }
 
 export function useTasksDispatch() {
-    const context = useContext(TaskDispatchContext);
-    if (context === undefined) {
-        throw new Error("useTasksDispatch must be used within a TaskProvider");
-    }
-    return context;
+  const context = useContext(TaskDispatchContext);
+  if (context === undefined) {
+    throw new Error("useTasksDispatch must be used within a TaskProvider");
+  }
+  return context;
 }
 
 
--- src/services/taskService.js
+++ src/services/taskService.js
@@ -1,10 +1,11 @@
-const API_URL = 'https://jsonplaceholder.typicode.com/todos';
+// const API_URL = 'https://jsonplaceholder.typicode.com/todos';
+const API_URL = 'http://localhost:3001/tasks';
 
 export const taskService = {
     async getAll() {
         try {
             const response = await fetch(`${API_URL}?_limit=10`);
-            if(!response.ok) throw new Error('Ошибка получения api');
+            if (!response.ok) throw new Error('Ошибка получения api');
             return await response.json()
         } catch (error) {
             console.log("Failed to fetch tasks: ", error)
@@ -16,19 +17,24 @@ export const taskService = {
             const response = await fetch(`${API_URL}`, {
                 method: "POST",
                 body: JSON.stringify({
-                    userId: crypto.randomUUID(),
                     title: newTaskData.title,
-                    completed: false,
-                    priority: "low",
-                    createdAt: new Date().getTime(),
+                    isCompleted: newTaskData.isCompleted || false,
+                    status: newTaskData.status || "not_started",
+                    priority: newTaskData.priority || "low",
+                    createdAt: Date.now(),
 
                 }),
-                 headers: {
+                headers: {
                     'Content-Type': 'application/json',
                 },
             })
+            // проверяем статус ответа, затем распарсить json, вернуть данные
+            if (!response.ok) throw new Error("Ошибка создания задачи");
+            const createdTask = await response.json();
+            return createdTask;
         } catch (error) {
             console.log(" Failed to create task", error)
         }
+
     },
 }
